00fdf0a455248f6c925f1ead9aeb9ab51a132cc8
AlexAlvCe <49877987+AlexAlvCe@users.noreply.github.com>
2021-04-16 20:27:36 -0600

    Update CrudArticulo

--- shopping-app/database/CrudArticulo.sql
+++ shopping-app/database/CrudArticulo.sql
@@ -13,15 +13,15 @@ BEGIN
 	if ((COALESCE(TRIM(_nombre),'') = '') OR (COALESCE(TRIM(_descripccion),'') = '') OR (_precio IS NULL) OR 
 		(_cantidad IS NULL) OR (_categoriaId IS NULL) OR (_sucursalId IS NULL)) then
 		RAISE 'Error: Null parameter';
-	ELSIF EXISTS(SELECT * FROM Articulo WHERE nombre = _nombre AND categoriaId = _categoriaId AND sucursalId = _sucursalId ) THEN
+	ELSIF EXISTS(SELECT ar.* FROM Articulo ar WHERE ar.nombre = _nombre AND ar.categoriaId = _categoriaId AND ar.sucursalId = _sucursalId ) THEN
 		RAISE 'Error:that Categoria already exists, please try another.';
-	ELSIF NOT EXISTS(SELECT * FROM categoria WHERE id = _categoriaId) THEN
+	ELSIF NOT EXISTS(SELECT ca.* FROM categoria ca WHERE ca.id = _categoriaId) THEN
 		RAISE 'Error: categoria doesn''t exists.';
-	ELSIF NOT EXISTS(SELECT * FROM sucursal WHERE id = _sucursalId) THEN
+	ELSIF NOT EXISTS(SELECT s.* FROM sucursal s WHERE s.id = _sucursalId) THEN
 		RAISE 'Error: sucursal doesn''t exists.';
 	else
 		BEGIN
-			INSERT INTO Articulo VALUES
+			INSERT INTO Articulo(nombre,descripccion,precio,cantidad,categoriaId,sucursalId) VALUES
 			(_nombre, _descripccion, _precio, _cantidad, _categoriaId, _sucursalId);
 			COMMIT;
 		END;
@@ -38,8 +38,10 @@ RETURNS TABLE (
 	categoriaId	int,
 	sucursalId	int
 ) AS $$
-	SELECT id, nombre, descripccion, precio, cantidad, categoriaId, sucursalId from Articulo;
-$$ LANGUAGE SQL;
+begin
+	return query SELECT  Ar.id,  Ar.nombre,  Ar.descripccion,  Ar.precio,  Ar.cantidad,  Ar.categoriaId,  Ar.sucursalId from Articulo Ar;
+end;
+$$ LANGUAGE PLPGSQL;
 
  
 CREATE or replace FUNCTION getAllArticulosSucursal(_idSucursal int ) 
@@ -52,8 +54,10 @@ RETURNS TABLE (
 	categoriaId	int,
 	sucursalId	int
 ) AS $$
-	SELECT id, nombre, descripccion, precio, cantidad, categoriaId, sucursalId from Articulo where _idSucursal = sucursalId ;
-$$ LANGUAGE SQL;
+begin
+	return query SELECT Ar.id, Ar.nombre, Ar.descripccion, Ar.precio, Ar.cantidad, Ar.categoriaId, Ar.sucursalId from Articulo Ar where _idSucursal = Ar.sucursalId ;
+end;
+$$ LANGUAGE PLPGSQL;
 
 
 
@@ -67,8 +71,10 @@ RETURNS TABLE (
 	categoriaId	int,
 	sucursalId	int
 ) AS $$
-	SELECT id, nombre, descripccion, precio, cantidad, categoriaId, sucursalId from Articulo where getCategoriaId(nombCat) = categoriaId and sucursalId = _sucursalId ;
-$$ LANGUAGE SQL;
+begin
+	return query SELECT Ar.id, Ar.nombre, Ar.descripccion, Ar.precio, Ar.cantidad, Ar.categoriaId, Ar.sucursalId from Articulo Ar where getCategoriaId(nombCat) = AR.categoriaId and Ar.sucursalId = _sucursalId ;
+end;
+$$ LANGUAGE PLPGSQL;
 
 create or replace function getArticuloWhitNomb(nombArt varchar(50),_sucursalId int )
 RETURNS TABLE (
@@ -80,8 +86,10 @@ RETURNS TABLE (
 	categoriaId	int,
 	sucursalId	int
 ) AS $$
-	SELECT id, nombre, descripccion, precio, cantidad, categoriaId, sucursalId from Articulo where nombArt  = nombre and sucursalId = _sucursalId ;
-$$ LANGUAGE SQL;
+begin 
+	return query SELECT AR.id, AR.nombre, AR.descripccion, AR.precio, AR.cantidad, AR.categoriaId, AR.sucursalId from Articulo AR where nombArt  = AR.nombre and AR.sucursalId = _sucursalId ;
+end;
+$$ LANGUAGE PLPGSQL;
 
 
 CREATE or replace PROCEDURE sp_updateArticulo(
@@ -98,16 +106,16 @@ BEGIN
     if ((_id IS NULL) OR (COALESCE(TRIM(_nombre),'') = '') OR (COALESCE(TRIM(_descripccion),'') = '') OR (_precio IS NULL) OR 
 		(_cantidad IS NULL) OR (_categoriaId IS NULL) OR (_sucursalId IS NULL)) then
 		RAISE 'Error: Null parameter';
-	ELSIF NOT EXISTS(SELECT * FROM Articulo WHERE id = _id) THEN
+	ELSIF NOT EXISTS(SELECT ar.* FROM Articulo ar WHERE ar.id = _id) THEN
 		RAISE 'Error: Articulo doesn''t exists.';
 		
-	ELSIF NOT EXISTS(SELECT * FROM Sucursal WHERE id = _sucursalId) THEN
+	ELSIF NOT EXISTS(SELECT s.* FROM Sucursal s WHERE s.id = _sucursalId) THEN
 		RAISE 'Error: Sucursal doesn''t exists.';	
 	
-	ELSIF NOT EXISTS(SELECT * FROM Categoria WHERE id = _categoriaId) THEN
+	ELSIF NOT EXISTS(SELECT ca.* FROM Categoria ca WHERE ca.id = _categoriaId) THEN
 		RAISE 'Error: Categoria doesn''t exists.';	
 	
-	ELSIF EXISTS(SELECT * FROM Articulo WHERE nombre = _nombre and id != _id ) THEN
+	ELSIF EXISTS(SELECT ar.* FROM Articulo ar WHERE ar.nombre = _nombre and ar.id != _id ) THEN
 		RAISE 'Error:that Articulo name is already in use in other instance, please try another.';
 	
 	ELSE
@@ -131,12 +139,12 @@ CREATE or replace PROCEDURE sp_deleteArticulo(
 )
 LANGUAGE PLPGSQL AS $$
 BEGIN
-	IF NOT EXISTS(SELECT * FROM Articulo WHERE id = _id) THEN
+	IF NOT EXISTS(SELECT ar.* FROM Articulo ar WHERE ar.id = _id) THEN
 		RAISE 'Error: Articulo not exists.';
 			
 	ELSE
 		BEGIN
-			DELETE FROM Articulo WHERE id = _id;
+			DELETE FROM Articulo Ar WHERE Ar.id = _id;
 			COMMIT;
 		END;
 	END IF;
